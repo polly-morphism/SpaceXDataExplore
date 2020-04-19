d475d3d81ed6323e0a3a29ec229ca8ba79f68f1b
Polina <35422513+polly-morphism@users.noreply.github.com>
2020-04-20 01:52:45 +0300

    Create populate.sql

---
+++ populate.sql
@@ -0,0 +1,36 @@
+INSERT INTO Customers(customer_id,name,type,country)
+VALUES(0, "DARPA", "Government", "United States" );
+INSERT INTO Customers(customer_id,name,type,country)
+VALUES(1, "ORS", "Government", "United States" );
+INSERT INTO Customers(customer_id,name,type,country)
+VALUES(2, "NASA", "Government", "United States" );
+INSERT INTO Customers(customer_id,name,type,country)
+VALUES(3, "Celestis", "Business", "United States" );
+INSERT INTO Customers(customer_id,name,type,country)
+VALUES(4, "ATSB", "Government", "Malaysia" );
+INSERT INTO Customers(customer_id,name,type,country)
+VALUES(5, "Orbcomm", "Business", "United States" );
+INSERT INTO Customers(customer_id,name,type,country)
+VALUES(6, "MDA Corp", "Business", "Canada" );
+INSERT INTO Customers(customer_id,name,type,country)
+VALUES(7, "SES", "Business", "Luxembourg" );
+INSERT INTO Customers(customer_id,name,type,country)
+VALUES(8, "Thaicom", "Business", "Thailand" );
+INSERT INTO Customers(customer_id,name,type,country)
+VALUES(9, "AsiaSat", "Business", "China" );
+INSERT INTO Customers(customer_id,name,type,country)
+VALUES(10, "US Air Force; NASA; NOAA", "Government", "United States" );
+INSERT INTO Customers(customer_id,name,type,country)
+VALUES(11, "Asia Broadcast Satellite", "Business", "Bermuda" );
+INSERT INTO Customers(customer_id,name,type,country)
+VALUES(12, "Eutelsat (Satmex)", "Business", "France (Mexico)" );
+INSERT INTO Customers(customer_id,name,type,country)
+VALUES(13, "Turkmenistan NSA", "Government", "Turkmenistan" );
+INSERT INTO Customers(customer_id,name,type,country)
+VALUES(14, "NASA; NOAA; CNES", "Government", "United States" );
+INSERT INTO Customers(customer_id,name,type,country)
+VALUES(15, "SKY Perfect JSAT Group", "Business", "Japan" );
+INSERT INTO Customers(customer_id,name,type,country)
+VALUES(16, "Spacecom", "Business", "Israel" );
+INSERT INTO Customers(customer_id,name,type,country)
+VALUES(17, "Iridium Communications", "Business", "United States" );
