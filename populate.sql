0859f16a846791516823228db36b670a5edd0553
Polina <35422513+polly-morphism@users.noreply.github.com>
2020-04-20 02:14:00 +0300

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -34,3 +34,85 @@ INSERT INTO Customers(customer_id,name,type,country)
 VALUES(16, "Spacecom", "Business", "Israel" );
 INSERT INTO Customers(customer_id,name,type,country)
 VALUES(17, "Iridium Communications", "Business", "United States" );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(0, "FalconSAT-2", "Research Satellite", 19.5 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(1, "DemoSat", "", 0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(2, "Trailblazer", "Communication Satellite", 0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(3, "PRESat, NanoSail-D", "Research Satellites", 8.0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(4, "Explorers", "Human Remains", 0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(5, "RatSat (DemoSat)", "", 165.0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(6, "RazakSAT", "Weather Satellite", 180.0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(7, "Dragon Spacecraft Qualification Unit", "", 0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(8, "SpaceX CRS (Dragon C1)", "Space Station Supplies", 0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(9, "SpaceX CRS (Dragon C2+)", "Space Station Supplies", 0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(10, "SpaceX CRS-1", "Space Station Supplies", 500.0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(11, "Orbcomm-OG2", "Communication Satellite", 150.0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(12, "SpaceX CRS-2", "Space Station Supplies", 677.0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(13, "CASSIOPE", "Communication/Research Satellite", 500.0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(14, "SES-8", "Communication Satellite", 3170.0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(15, "Thaicom 6", "Communication Satellite", 3325.0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(16, "SpaceX CRS-3", "Space Station Supplies", 2296.0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(17, "OG2 Mission 1 (6 OG2 Satellites)", "Communication Satellite", 0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(18, "AsiaSat 8", "Communication Satellite", 4535.0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(19, "AsiaSat 6", "Communication Satellite", 4428.0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(20, "SpaceX CRS-4", "Space Station Supplies", 2216.0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(21, "SpaceX CRS-5", "Space Station Supplies", 2395.0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(22, "DSCOVR", "Weather Satellite", 570.0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(23, "ABS-3A", "Communication Satellite", 4159.0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(24, "Eutelsat 115 West B (Satmex 7)", "Communication Satellite", 4159.0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(25, "SpaceX CRS-6", "Space Station Supplies", 1898.0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(26, "TurkmenAlem 52E/MonacoSAT", "Communication Satellite", 4707.0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(27, "SpaceX CRS-7", "Space Station Supplies", 1952.0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(28, "OG2 Mission 2 (11 OG2 Satellites)", "Communication Satellite", 0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(29, "Jason-3", "Weather Satellite", 553.0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(30, "SES-9", "Communication Satellite", 5271.0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(31, "SpaceX CRS-8", "Space Station Supplies", 3136.0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(32, "JCSAT-14", "Communication Satellite", 4696.0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(33, "Thaicom 8", "Communication Satellite", 3100.0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(34, "ABS-2A", "Communication Satellite", 3600.0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(35, "Eutelsat 117 West B (Satmex 9)", "Communication Satellite", 3600.0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(36, "SpaceX CRS-9", "Space Station Supplies", 2257.0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(37, "JCSAT-16", "Communication Satellite", 4600.0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(38, "Amos-6", "Communication Satellite", 5500.0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(39, "Iridium NEXT 1-10", "Communication Satellite", 9600.0 );
+INSERT INTO Payloads(payload_id,name,type,mass_kg)
+VALUES(40, "SpaceX CRS-10", "Space Station Supplies", 2490.0 );
