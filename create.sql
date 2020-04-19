1fc5d3ede0a4f591ec9fb1cd5d3ab86503dd0563
polly-morphism <polinasirko@gmail.com>
2020-04-20 00:27:03 +0300

    feat: create script

---
+++ create.sql
@@ -0,0 +1,85 @@
+CREATE TABLE Customers
+(
+  customer_id integer,
+  name char(20),
+  type char(20),
+  country char(20) 
+
+);
+
+ALTER TABLE Customers
+ADD CONSTRAINT customer_pk 
+    PRIMARY KEY (customer_id);
+
+-- 
+
+
+CREATE TABLE Payloads
+(
+  payload_id integer,
+  name char(40),
+  type char(35),
+  mass_kg  REAL
+);
+
+ALTER TABLE Payloads
+ADD CONSTRAINT payload_pk 
+    PRIMARY KEY (payload_id);
+
+-- 
+
+
+
+CREATE TABLE Vehicles
+(
+  vehicle_id integer,
+  type char(40)
+);
+
+ALTER TABLE Vehicles
+ADD CONSTRAINT vehicle_pk 
+    PRIMARY KEY (vehicle_id);
+
+-- 
+
+
+
+
+
+CREATE TABLE Missions
+(
+  mission_id integer,
+  vehicle_id integer,
+  payload_id integer,
+  customer_id integer,
+  mission_outcome char(20),
+  failure_reason char(40),
+  landing_type char(20),
+  landing_outcome char(20),
+  flight_number char(5),
+  launch_date date,
+  launch_site char(40),
+  orbit char(40)
+);
+
+ALTER TABLE Missions
+ADD CONSTRAINT mission_pk 
+    PRIMARY KEY (mission_id);
+  
+
+ALTER TABLE Missions
+ADD CONSTRAINT vehicle_pk
+  FOREIGN KEY (vehicle_id)
+  REFERENCES Vehicles (vehicle_id);
+
+
+ALTER TABLE Missions
+ADD CONSTRAINT payload_pk
+  FOREIGN KEY (payload_id)
+  REFERENCES Payloads (payload_id);
+
+
+ALTER TABLE Missions
+ADD CONSTRAINT customer_pk
+  FOREIGN KEY (customer_id)
+  REFERENCES Customers (customer_id);
